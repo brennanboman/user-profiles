eb6b2f044cd313b52995c8805fdbc988a41e9a92
Brennan Boman <brennanboman@Brennans-MacBook-Air.local>
2017-03-11 12:47:01 -0700

    a bit farther

--- server/server.js
+++ server/server.js
@@ -4,16 +4,19 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var cors = require('cors');
 var config = require('../config.js')
+var profileCtrl = require('../controllers/profileCtrl.js');
+var userCtrl = require('../controllers/userCtrl.js')
 var port = 3000;
 
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
+app.use(session({ secret: config.sessionSecret  }));
 
 var corsOptions = {
   origin: 'http://localhost:3000'
 }
 
-
+app.post('/api/login', userCtrl.login);
 
 
 
